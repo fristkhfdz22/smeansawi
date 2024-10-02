e96c8005f8b4acb40ce4a7814b5084961c04e682
fristkhfdz22 <khafidzrizziqalhuda@gmail.com>
2024-10-02 08:12:30 -0700

    memabuat db tampilan dan admin

---
+++ application/views/admin/layout/v_footer.php
@@ -0,0 +1,23 @@
+</div>
+                    <!-- /.row -->
+                </div>
+</div>
+            <!-- /#page-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <!-- jQuery -->
+        <script src="<?= base_url() ?>template/backend/js/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="<?= base_url() ?>template/backend/js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="<?= base_url() ?>template/backend/js/metisMenu.min.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="<?= base_url() ?>template/backend/js/startmin.js"></script>
+
+    </body>
+</html>
---
+++ application/views/admin/layout/v_header.php
@@ -0,0 +1,30 @@
+<body>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="index.html"><?= $title ?></a>
+        </div>
+
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+      
+
+        <ul class="nav navbar-right navbar-top-links">
+            <li class="dropdown navbar-inverse">
+           
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i> nama user <b></b>
+                </a>
+             
+            </li>
+        </ul>
+<body>
